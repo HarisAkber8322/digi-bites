0c9658256701e3d09365153ed527bdd95ccdf1cf
Ali <ahaider@yobo.dev>
2024-05-15 22:57:17 +0500

    adminLayout fixed

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -35,7 +35,7 @@ const Layout = (
         <SideBarComponent toggle={toggle}
         //  themeStore={themeStore} 
         />
-        <Div content={children} themeDivClasses={classNames(["ease-in-out duration-300 bg-lightGary w-full h-full pt-[64px]", toggle ? "ml-[250px]" : "ml-0"])} themeMode={themeStore.themeMode} />
+        <Div content={children} themeDivClasses={classNames(["ease-in-out duration-300 bg-lightGary w-full h-full pt-[64px]", toggle ? "ml-[250px]" : "ml-0"])} />
 
     </div >
     </>
