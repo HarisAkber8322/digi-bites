c9894a2dde1d94f2f2f7405c4815d52ddace89ed
Ali Haider <alihaiderhameed007@gmail.com>
2024-05-22 00:16:20 +0500

    errors resolve after pull

--- src/components/ClientComponent/OtherComponents/CategorySlider.tsx
+++ src/components/ClientComponent/OtherComponents/CategorySlider.tsx
@@ -1,51 +1,10 @@
-    // import React from "react";
-    // import { MenuList } from "../../../utills/constants";
-    // import Image from "next/image";
-    // import Text from "../../UI/Text";
-    // const CategorySlider = () => {
-    //   return (
-    //     <>
-    //       <div className="mt-10 flex justify-center">
-    //         <Text
-    //           themeDivClasses=" text-3xl font-bold  "
-    //           content={"All Category"}
-    //         />
-    //       </div>
-
-    //       <div className="flex mt-7 ml-20 mr-20 gap-6">
-    //         {MenuList.map((menuItem, index) => (
-    //           <div key={index}>
-    //             <div className="">
-    //               <Image
-    //                 className="rounded-full"
-    //                 src={menuItem.image}
-    //                 alt={menuItem.name}
-    //                 width={130}
-    //                 height={130}
-    //               ></Image>
-    //               <div className="flex justify-center">
-    //               <Text
-    //                 themeDivClasses=" text-center mt-2 font-medium "
-    //                 content={menuItem.name}
-    //               />
-    //               </div>
-                
-    //             </div>
-    //           </div>
-    //         ))}
-    //       </div>
-    //     </>
-    //   );
-    // };
-
-    // export default CategorySlider;
-
 import React from "react";
 import Slider from "react-slick";
 import { MenuList } from "../../../utills/constants";
 import Image from "next/image";
 import Text from "../../UI/Text";
 import Link from "next/link";
+import { observer } from "mobx-react";
 
 const CategorySlider = () => {
   const settings = {
@@ -110,5 +69,5 @@ const CategorySlider = () => {
     </>
   );
 };
-export default CategorySlider;
+export default observer(CategorySlider);
 
