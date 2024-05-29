ab5855fcc5fe24c34dd19c7ba8ff1ddbfc5a03f3
Ali Haider <alihaiderhameed007@gmail.com>
2024-05-30 01:42:57 +0500

    upadating count

---
+++ src/app/admin/dashboard/page.tsx
@@ -0,0 +1,18 @@
+// pages/client/burger.tsx
+"use client";
+import Div from '@/components/UI/Div';
+import React, { useState } from 'react';// Adjust according to your project structure
+
+const Dashboard: React.FC = () => {
+ 
+  return (
+    <Div
+      themeDivClasses={"w-[1180px] m-auto"}
+      content={
+       <>Dashboard </>
+      }
+    />
+  );
+};
+
+export default Dashboard;
